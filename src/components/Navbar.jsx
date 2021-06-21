2940d60330e23850e4a6afde2428f30a3359ce46
Palak Gupta <palakgupta2712@gmail.com>
2021-06-22 00:43:02 +0530

    Dropdown Menu added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,86 @@
 import React from "react";
+import styled from "styled-components";
+import logo from "../rgb.png";
+import DropdownMenu from "./dropdownMenu/DropdownMenu";
+import { AiFillGithub } from "react-icons/ai";
 
 function Navbar() {
-  return <div>NAVBAR</div>;
+  return (
+    <NavbarContainer>
+      <NavLogo>
+        <Logo src={logo} alt="logo" />
+        Tynts
+      </NavLogo>
+      <NavMenu>
+        <Button>
+          <AiFillGithub />
+          Github
+        </Button>
+        <DropdownMenu />
+      </NavMenu>
+    </NavbarContainer>
+  );
 }
 
 export default Navbar;
+
+const NavbarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  background-color: black;
+  color: white;
+  font-size: 18px;
+  height: 80px;
+  @media (max-width: 600px) {
+    flex-direction: column;
+    height: 100px;
+  }
+`;
+const NavLogo = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 50px;
+  border: 2px solid white;
+  padding: 10px;
+  letter-spacing: 6px;
+  @media (max-width: 600px) {
+    margin: 10px;
+    padding: 6px;
+  }
+  color: white;
+  font-weight: 800;
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  display: flex;
+  margin-right: 20px;
+  @media (max-width: 600px) {
+    margin: 2px;
+  }
+`;
+
+const Button = styled.button`
+  background: #ffffff;
+  border-radius: 90px;
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 5px;
+  margin: 0px 12px;
+  padding: 10px 17px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.3);
+  border: none;
+  vertical-align: middle;
+  transition: box-shadow 0.4s ease;
+  margin-left: auto;
+`;
+
+const Logo = styled.img`
+  height: 30px;
+  margin-right: 10px;
+`;
---
+++ src/components/dropdownMenu/DropdownMenu.jsx
@@ -0,0 +1,91 @@
+import React, { useRef } from "react";
+import "./styles.css";
+import { useClick } from "../useClick";
+import { IoIosArrowDropdownCircle } from "react-icons/io";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export default function App() {
+  const dropdownRef = useRef(null);
+  const [isActive, setIsActive] = useClick(dropdownRef, false);
+  const onClick = () => setIsActive(!isActive);
+
+  return (
+    <React.Fragment>
+      <Menu>
+        <MenuButton onClick={onClick}>
+          <span>User</span>
+          <IoIosArrowDropdownCircle />
+        </MenuButton>
+        <nav
+          ref={dropdownRef}
+          className={`menu ${isActive ? "active" : "inactive"}`}
+        >
+          <ul>
+            <List>
+              <Link to="/color-manipulator" onClick={onClick}>
+                Manipulator
+              </Link>
+            </List>
+            <List>
+              <Link to="/color-gradients" onClick={onClick}>
+                Gradients
+              </Link>
+            </List>
+            <List>
+              <Link to="/shades-and-tints" onClick={onClick}>
+                Shades&Tints
+              </Link>
+            </List>
+            <List>
+              <Link to="/color-harmonies" onClick={onClick}>
+                Harmonies
+              </Link>
+            </List>
+          </ul>
+        </nav>
+      </Menu>
+    </React.Fragment>
+  );
+}
+
+const Menu = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+  /* margin-left: 100px; */
+`;
+
+const MenuButton = styled.button`
+  background: #ffffff;
+  border-radius: 90px;
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 17px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.3);
+  border: none;
+  vertical-align: middle;
+  transition: box-shadow 0.4s ease;
+  margin-left: auto;
+  gap: 5px;
+`;
+
+const List = styled.li`
+  border-bottom: 1px solid #dddddd;
+
+  a {
+    text-decoration: none;
+    color: #333333;
+    padding: 15px 12px;
+    display: block;
+    @media (max-width: 600px) {
+      padding: 10px 12px;
+    }
+  }
+  &:hover {
+    background-color: #eeefff;
+  }
+`;
